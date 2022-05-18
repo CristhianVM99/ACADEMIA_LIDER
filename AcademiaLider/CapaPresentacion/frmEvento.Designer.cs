3b2accb23198faddb4219243ad0828823a25886f
Cristhian Villca Mamani <cristhianvillcamamani@gmail.com>
2022-05-18 13:13:08 -0600

    Docentes y Eventos.

---
+++ AcademiaLider/CapaPresentacion/frmEvento.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace AcademiaLider.CapaPresentacion
+{
+    partial class frmEvento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCriterioBusqueda = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.cboModalidad = new System.Windows.Forms.ComboBox();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpFechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cboCiudad = new System.Windows.Forms.ComboBox();
+            this.cboDocente = new System.Windows.Forms.ComboBox();
+            this.txtCargaHoraria = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCriterioBusqueda);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.Location = new System.Drawing.Point(14, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(838, 58);
+            this.groupBox1.TabIndex = 72;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtCriterioBusqueda
+            // 
+            this.txtCriterioBusqueda.Location = new System.Drawing.Point(566, 21);
+            this.txtCriterioBusqueda.Name = "txtCriterioBusqueda";
+            this.txtCriterioBusqueda.Size = new System.Drawing.Size(185, 20);
+            this.txtCriterioBusqueda.TabIndex = 5;
+            this.txtCriterioBusqueda.TextChanged += new System.EventHandler(this.txtCriterioBusqueda_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(757, 19);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(250, 19);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(169, 19);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(88, 19);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(7, 19);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(11, 10);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(74, 20);
+            this.lblTitulo.TabIndex = 71;
+            this.lblTitulo.Text = "Eventos";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(357, 99);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 70;
+            this.label10.Text = "Carga Horaria";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(357, 73);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 69;
+            this.label9.Text = "Docente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "Estado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Modalidad";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(124, 44);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(211, 20);
+            this.txtNombres.TabIndex = 55;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 54;
+            this.label1.Text = "Nombres";
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(14, 238);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.Size = new System.Drawing.Size(838, 270);
+            this.dgvListado.TabIndex = 53;
+            this.dgvListado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvListado_CellContentClick);
+            this.dgvListado.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvListado_RowHeaderMouseDoubleClick);
+            // 
+            // cboModalidad
+            // 
+            this.cboModalidad.FormattingEnabled = true;
+            this.cboModalidad.Location = new System.Drawing.Point(124, 69);
+            this.cboModalidad.Name = "cboModalidad";
+            this.cboModalidad.Size = new System.Drawing.Size(211, 21);
+            this.cboModalidad.TabIndex = 75;
+            // 
+            // cboEstado
+            // 
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Items.AddRange(new object[] {
+            "ACTIVO",
+            "INACTIVO"});
+            this.cboEstado.Location = new System.Drawing.Point(124, 95);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(211, 21);
+            this.cboEstado.TabIndex = 76;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 78;
+            this.label5.Text = "Fecha de inicio";
+            // 
+            // dtpFechaInicio
+            // 
+            this.dtpFechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaInicio.Location = new System.Drawing.Point(125, 122);
+            this.dtpFechaInicio.Name = "dtpFechaInicio";
+            this.dtpFechaInicio.Size = new System.Drawing.Size(139, 20);
+            this.dtpFechaInicio.TabIndex = 77;
+            this.dtpFechaInicio.Value = new System.DateTime(2022, 5, 12, 0, 0, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 80;
+            this.label2.Text = "Fecha de fin";
+            // 
+            // dtpFechaFinal
+            // 
+            this.dtpFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaFinal.Location = new System.Drawing.Point(125, 148);
+            this.dtpFechaFinal.Name = "dtpFechaFinal";
+            this.dtpFechaFinal.Size = new System.Drawing.Size(139, 20);
+            this.dtpFechaFinal.TabIndex = 79;
+            this.dtpFechaFinal.Value = new System.DateTime(2022, 5, 12, 0, 0, 0, 0);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(354, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 13);
+            this.label6.TabIndex = 82;
+            this.label6.Text = "Ciudad  de procedencia";
+            // 
+            // cboCiudad
+            // 
+            this.cboCiudad.FormattingEnabled = true;
+            this.cboCiudad.Location = new System.Drawing.Point(480, 44);
+            this.cboCiudad.Name = "cboCiudad";
+            this.cboCiudad.Size = new System.Drawing.Size(211, 21);
+            this.cboCiudad.TabIndex = 81;
+            // 
+            // cboDocente
+            // 
+            this.cboDocente.FormattingEnabled = true;
+            this.cboDocente.Location = new System.Drawing.Point(480, 73);
+            this.cboDocente.Name = "cboDocente";
+            this.cboDocente.Size = new System.Drawing.Size(211, 21);
+            this.cboDocente.TabIndex = 83;
+            // 
+            // txtCargaHoraria
+            // 
+            this.txtCargaHoraria.Location = new System.Drawing.Point(480, 101);
+            this.txtCargaHoraria.Name = "txtCargaHoraria";
+            this.txtCargaHoraria.Size = new System.Drawing.Size(211, 20);
+            this.txtCargaHoraria.TabIndex = 84;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(697, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 85;
+            this.label7.Text = "Hrs.";
+            // 
+            // frmEvento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCargaHoraria);
+            this.Controls.Add(this.cboDocente);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cboCiudad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpFechaFinal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpFechaInicio);
+            this.Controls.Add(this.cboEstado);
+            this.Controls.Add(this.cboModalidad);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvListado);
+            this.Name = "frmEvento";
+            this.Text = "frmEventos";
+            this.Load += new System.EventHandler(this.frmEvento_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCriterioBusqueda;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.ComboBox cboModalidad;
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpFechaInicio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFechaFinal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboCiudad;
+        private System.Windows.Forms.ComboBox cboDocente;
+        private System.Windows.Forms.TextBox txtCargaHoraria;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ AcademiaLider/CapaPresentacion/frmEvento.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AcademiaLider.CapaLogicaNegocio;
+using AcademiaLider.Entidades;
+
+namespace AcademiaLider.CapaPresentacion
+{
+    public partial class frmEvento : Form
+    {
+        private ClnEvento objLogicaNegocio = new ClnEvento();
+        private Evento objEvento = new Evento();
+
+        public frmEvento()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            DeshabilitarBtnNuevo();
+            Inicializar();
+            CargarDatos();
+            objLogicaNegocio.CrearRegistro(objEvento);
+            if (objLogicaNegocio.Estado)
+            {
+                Limpiar();
+                Listar();
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+            else
+            {
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+            HabilitarBtnNuevo();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarBtnModificar();
+            DeshabilitarBtnEliminar();
+            CargarDatos();
+            objLogicaNegocio.ModificarRegistro(objEvento);
+            if (objLogicaNegocio.Estado)
+            {
+                Limpiar();
+                HabilitarBtnNuevo();
+                Listar();
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+            else
+            {
+                HabilitarBtnModificar();
+                HabilitarBtnEliminar();
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarBtnModificar();
+            DeshabilitarBtnEliminar();
+            objLogicaNegocio.EliminarRegistro(objEvento.Codigo);
+            if (objLogicaNegocio.Estado)
+            {
+                Limpiar();
+                HabilitarBtnNuevo();
+                Listar();
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+            else
+            {
+                HabilitarBtnModificar();
+                HabilitarBtnEliminar();
+                MessageBox.Show(objLogicaNegocio.Mensaje);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            Inicializar();
+            HabilitarBtnNuevo();
+            DeshabilitarBtnModificar();
+            DeshabilitarBtnEliminar();
+        }
+
+        private void txtCriterioBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            String criterio = txtCriterioBusqueda.Text;
+            dgvListado.DataSource = objLogicaNegocio.BuscarRegistros(criterio);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            String criterio = txtCriterioBusqueda.Text;
+            dgvListado.DataSource = objLogicaNegocio.BuscarRegistros(criterio);
+        }
+
+        private void Listar()
+        {
+            dgvListado.DataSource = objLogicaNegocio.ListarRegistros();
+        }
+
+        private void frmEvento_Load(object sender, EventArgs e)
+        {
+            Limpiar();
+            DeshabilitarBtnModificar();
+            DeshabilitarBtnEliminar();
+            CargarModalidades();
+            CargarCiudades();
+            CargarDocentes();
+            Listar();
+        }
+
+        private void Limpiar()
+        {
+            txtNombres.Text = "";
+            cboModalidad.Text = "";
+            txtCargaHoraria.Text = "";
+            dtpFechaInicio.Text = "";
+            dtpFechaFinal.Text = "";
+            cboCiudad.Text = "";
+            cboEstado.Text = "";
+            cboDocente.Text = "";
+        }
+
+        private void Inicializar()
+        {
+            objEvento = new Evento();
+        }
+
+        private void HabilitarBtnNuevo()
+        {
+            btnNuevo.Enabled = true;
+        }
+
+        private void DeshabilitarBtnNuevo()
+        {
+            btnNuevo.Enabled = false;
+        }
+
+        private void HabilitarBtnModificar()
+        {
+            btnModificar.Enabled = true;
+        }
+
+        private void DeshabilitarBtnModificar()
+        {
+            btnModificar.Enabled = false;
+        }
+
+        private void HabilitarBtnEliminar()
+        {
+            btnEliminar.Enabled = true;
+        }
+
+        private void DeshabilitarBtnEliminar()
+        {
+            btnEliminar.Enabled = false;
+        }
+
+        private void CargarModalidades()
+        {
+            cboModalidad.ValueMember = "codigo";
+            cboModalidad.DisplayMember = "nombre";
+            cboModalidad.DataSource = objLogicaNegocio.ListarModalidades();
+        }
+
+        private void CargarCiudades()
+        {
+            cboCiudad.ValueMember = "codigo";
+            cboCiudad.DisplayMember = "nombre";
+            cboCiudad.DataSource = objLogicaNegocio.ListarCiudades();
+        }
+
+        private void CargarDocentes()
+        {
+            cboDocente.ValueMember = "codigo";
+            cboDocente.DisplayMember = "nombre";
+            cboDocente.DataSource = objLogicaNegocio.ListarDocentes();
+        }
+
+        private void CargarDatos()
+        {
+            objEvento.Nombre = txtNombres.Text;
+            objEvento.CodModalidad = Convert.ToInt32(cboModalidad.SelectedValue);
+            objEvento.CargaHoraria = Convert.ToInt32(txtCargaHoraria.Text);
+            objEvento.FechaInicio = dtpFechaInicio.Value.Year + "-" + dtpFechaInicio.Value.Month + "-" + dtpFechaInicio.Value.Day;
+            objEvento.FechaFinal = dtpFechaFinal.Value.Year + "-" + dtpFechaFinal.Value.Month + "-" + dtpFechaFinal.Value.Day;
+            objEvento.CodCiudad = Convert.ToInt32(cboCiudad.SelectedValue);
+            objEvento.Estado = cboEstado.Text;
+            objEvento.CodDocente = Convert.ToString(cboDocente.SelectedValue);
+        }
+
+        private void MostrarDatos()
+        {
+            txtNombres.Text = objEvento.Nombre;
+            cboModalidad.SelectedValue = objEvento.CodModalidad;
+            txtCargaHoraria.Text = objEvento.CargaHoraria.ToString();
+            dtpFechaInicio.Text = objEvento.FechaInicio;
+            dtpFechaFinal.Text = objEvento.FechaFinal;
+            cboCiudad.SelectedValue = objEvento.CodCiudad;
+            cboEstado.Text = objEvento.Estado;
+            cboDocente.SelectedValue = objEvento.CodDocente;
+        }
+
+        private void dgvListado_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dgvListado.Rows.Count - 1)
+            {
+                String codigo = dgvListado.Rows[e.RowIndex].Cells[0].Value.ToString();
+                objEvento = objLogicaNegocio.ObtenerRegistro(codigo);
+                DeshabilitarBtnNuevo();
+                HabilitarBtnModificar();
+                HabilitarBtnEliminar();
+                MostrarDatos();
+            }
+        }
+
+        private void dgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
